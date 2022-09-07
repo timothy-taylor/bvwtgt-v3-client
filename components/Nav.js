1727cb769eb5860bcd4872039ad6118d4a941eea
Tim <timothy.george.taylor@gmail.com>
2022-09-07 11:10:46 -0700

    ADD: active link styling

--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const links = [
@@ -5,14 +6,22 @@ const links = [
   { text: "About", path: "/about" },
 ]
 
+const isActiveStyle = (routerPath, linkPath) => (
+  routerPath === linkPath ? "pr-4 lowercase font-bold" : "pr-4 lowercase"
+)
+
 export default function Nav() {
+  const router = useRouter();
+
   return (
     <nav className="absolute top-1 right-4 md:right-1/4">
       <ul className="list-none m-0">
         {links.map((link,i) => (
           <li className="inline p-0" key={`${link.text}${i}`}>
             <Link href={link.path}>
-              <a className="pr-4 lowercase">{link.text}</a>
+              <a className={isActiveStyle(router.asPath, link.path)}>
+                {link.text}
+              </a>
             </Link>
           </li>
         ))}
